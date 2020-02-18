b7eb88c935f4b897f90a536a69fe2cf101b5045e
balazsfalvi <balazsfalvi12@gmail.com>
2020-02-18 18:28:05 +0100

    Basics and landing page

---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -0,0 +1,42 @@
+package net.kzn.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring Web MVC");
+		return mv;
+	}
+	
+	/*//Request param bemutatása
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting) {
+		if(greeting==null) {
+			greeting="Hell";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	//PATHVARIABLE
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting) {
+		if(greeting==null) {
+			greeting="Hell";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}*/
+	
+	}
+	
+
